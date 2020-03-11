57bd63093dcf481e7373722379dacc328cc9fcdf
shihmin-18646 <shihmin-18646@email.esunbank.com.tw>
2020-03-11 16:11:47 +0800

    lesson3 顧客查詢表單雛形

--- src/app/client/client-search/client-search.component.ts
+++ src/app/client/client-search/client-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-client-search',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientSearchComponent implements OnInit {
 
+  ClientForm = new FormGroup({
+    ID: new FormControl(),
+    CustomerName: new FormControl(),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleOnSubmit() {
+    console.log('Your form data : ', this.ClientForm.value );
   }
 
 }
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ClientSearchComponent } from './client-search/client-search.component';
+import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
 
 
 
@@ -9,6 +10,8 @@ import { ClientSearchComponent } from './client-search/client-search.component';
   declarations: [ClientSearchComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       { path: '', redirectTo: 'search', pathMatch: 'full' },
       { path: 'search', component: ClientSearchComponent}

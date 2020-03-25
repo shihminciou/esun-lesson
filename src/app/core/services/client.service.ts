a97ec967efd339356815f25ecaf765199f1affd5
shihmin-18646 <shihmin-18646@email.esunbank.com.tw>
2020-03-25 08:48:26 +0800

    environment apiurl變數

--- src/app/core/services/client.service.ts
+++ src/app/core/services/client.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  apiUrl = 'https://www.mocky.io/v2/5e79bc6230000098009301ca';
+  apiService =  environment.api_url + '/5e79bc6230000098009301ca';
 
   constructor(
     private http: HttpClient,
   ) { }
 
   query() {
-    return this.http.get(this.apiUrl);
+    return this.http.get(this.apiService);
   }
 }
